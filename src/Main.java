1a2cb09c39f2562c2bfc99c12b6e61ea43cddae1
AndersenASG <andersenasg88@gmail.com>
2023-08-04 21:10:31 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    private static void findDuplicationNumber{int[] numbers} {
+//        code
+    }
+
+    public static void main(String[] args) {
+        int[] input1 = {1,1};
+        findDuplicationNumber(input1) // [1]
+
+        int[] input2 = {1,2,3,4};
+        findDuplicationNumber(input2 // []
+
+        int[] input3 = {1, 2, 1, 2, 2, 3, 4, 5, 5, 5, 5};
+        findDuplicationNumber(input3) // [1, 2, 5]
+    }
+}
